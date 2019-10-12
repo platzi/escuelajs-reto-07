37e9f65a89137ce7176b03414b3867c7d79960d6
Miguel castillo <mcastillo@360pmi.com>
2019-10-12 10:39:20 -0500

    reto parte 1

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFound = () => (
+  <>
+    <section className="error">
+      <h2>404</h2>
+      <p>Página no econtrada</p>
+      <p><a href="/">volver al inicio</a></p>
+    </section>
+  </>
+);
+
+export default NotFound;
