3d26c50a7ecdc94ae71416bca5ce59b042fb3045
Cristobal Vega <cristobalvega96@gmail.com>
2019-10-12 12:23:07 -0500

    Tercer Reto
    Ahora se pueden eliminar los elementos del carrito

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,18 @@
 const reducer = (state, action) => {
   switch (action.type) {
+    case 'DELETE_ITEM':
+      return {
+        ...state,
+        cart: state.cart.filter(items => items.id !== action.payload)
+      };
     case 'ADD_TO_CART':
       return {
         ...state,
         cart: [...state.cart, action.payload]
-      }
+      };
     default:
       return state;
   }
-}
+};
 
 export default reducer;
