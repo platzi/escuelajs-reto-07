9c7451d696b241723212fb14dba335cf3380e0b0
Gerardo Nava Pereda <gerardo.nava.pereda@gmail.com>
2019-10-12 11:02:04 -0500

    Solved first challenge, Page 404 not found and webpack config publicPack and devServer

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,10 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+    publicPath: '/',
+  },
+  devServer: {
+    historyApiFallback: true,
   },
   resolve: {
     extensions: ['.js', '.jsx'],
