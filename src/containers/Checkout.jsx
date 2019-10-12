d9a6b5dfa2fafacb4a1d431ca564f0335a1a1311
Abdiel <abdielalvarez@icloud.com>
2019-10-12 14:40:21 -0500

    eto terminado

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { deleteToCart } from '../actions';
 import '../styles/components/Checkout.styl';
 
 const Checkout = (props) => {
   const { cart } = props;
+  const handleDeleteCart = (index) => {
+    props.deleteToCart(index);
+  }
+  const total = cart.reduce(
+    (totalValue, currentValue) => totalValue + currentValue.price,
+    0
+  )
   return (
     <div className="Checkout">
       <div className="Checkout-content">
         {cart.length > 0 ? <h3>Lista de Pedidos:</h3> : <h2>Sin Pedidos</h2>}
-        {cart.map(item => (
-          <div className="Checkout-item">
+        {cart.map((item, index) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <div key={index} className="Checkout-item">
             <div className="Checkout-element">
               <h4>{item.title}</h4>
               <span>
@@ -17,14 +26,17 @@ const Checkout = (props) => {
                 {item.price}
               </span>
             </div>
-            <i className="fas fa-trash-alt" />
+            <button type='button' onClick={() => handleDeleteCart(index)} className="fas fa-trash-alt" />
           </div>
         ))}
       </div>
       {cart.length > 0 && (
         <div className="Checkout-sidebar">
           <h3>Precio Total:</h3>
-          <h4>$</h4>
+          <h4>
+            $
+            {total}
+          </h4>
         </div>
       )}
     </div>
@@ -37,4 +49,8 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps, null)(Checkout);
+const mapDispatchToProps = {
+  deleteToCart,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Checkout);
