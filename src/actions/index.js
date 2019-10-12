010d2810c26e42b1019aac196d11503b44f32ab5
Edwin García <spark.com.co@gmail.com>
2019-10-12 11:05:35 -0500

    cuarto problema

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,9 @@
-const addToCart = payload => ({
+export const addToCart = payload => ({
   type: 'ADD_TO_CART',
   payload,
 });
 
-export { addToCart };
+export const removeProduct = payload => ({
+  type: 'REMOVE_PRODUCT',
+  payload,
+});
