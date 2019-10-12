a547fa15f75e3036e92db7a82af84e9437f9e24a
Julian Toro <jtoro@tecmovin.com>
2019-10-12 10:46:45 -0500

    reto 1 reto 2 reto 3 reto 4

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -31,7 +31,10 @@ const Checkout = (props) => {
       {cart.length > 0 && (
         <div className="Checkout-sidebar">
           <h3>Precio Total:</h3>
-          <h4>$</h4>
+          <h4> 
+            $
+            {cart.reduce((acum, item) => acum+item.price, 0 )}
+          </h4>
         </div>
       )}
     </div>
