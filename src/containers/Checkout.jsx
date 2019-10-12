cf37a82c90238c6ae1f62d353eab54bb22965e4b
luisrdz5 <luis@luisrodriguez.com>
2019-10-12 15:49:15 -0500

    solved the third problem

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import '../styles/components/Checkout.styl';
+import { deleteFromCart }  from '../actions';
 
 const Checkout = (props) => {
   const { cart } = props;
+  const handleDeleteElement = (itemId) => {
+    props.deleteFromCart(itemId);
+  };
   return (
     <div className="Checkout">
       <div className="Checkout-content">
         {cart.length > 0 ? <h3>Lista de Pedidos:</h3> : <h2>Sin Pedidos</h2>}
         {cart.map(item => (
-          <div className="Checkout-item">
+          <div className="Checkout-item" key={item.id}>
             <div className="Checkout-element">
               <h4>{item.title}</h4>
               <span>
@@ -17,7 +21,8 @@ const Checkout = (props) => {
                 {item.price}
               </span>
             </div>
-            <i className="fas fa-trash-alt" />
+            <i className="fas fa-trash-alt" onClick={() => handleDeleteElement(item.id)} />
+            
           </div>
         ))}
       </div>
@@ -36,5 +41,8 @@ const mapStateToProps = state => {
     cart: state.cart,
   };
 };
+const mapDispatchToProps = {
+  deleteFromCart,
+};
 
-export default connect(mapStateToProps, null)(Checkout);
+export default connect(mapStateToProps, mapDispatchToProps)(Checkout);
