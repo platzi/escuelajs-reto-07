371013fde70fda5c63fe1a4342aa3d07e50d03a3
sue <sue@MacBook-Pro-de-sue.local>
2019-10-12 11:22:11 -0500

    configuring webpack and creating not found component and route

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/components/notFound.styl';
+
+const NotFound = () => {
+  return (
+    <div className="notFound">
+      <h1>
+        Sorry, Not found baby.
+      </h1>
+    </div>
+  )
+};
+export default NotFound;
