bad49ecff38c25965ba5beb91e25e03a7259a27b
Fabio Cortes <fcor14@hotmail.com>
2019-10-12 10:56:51 -0500

    reto 2

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,9 @@
-const addToCart = payload => ({
-  type: 'ADD_TO_CART',
-  payload,
-});
+const actions = {
+  addToCart: payload => ({
+    type: 'ADD_TO_CART',
+    payload,
+  }),
+}
 
-export { addToCart };
+
+export default actions;
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { addToCart } from '../actions';
+import actions from '../actions';
 import '../styles/components/Products.styl';
 
 const Products = (props) => {
@@ -41,7 +41,7 @@ const mapStateToProps = state => {
 };
 
 const mapDispatchToProps = {
-  addToCart,
+  addToCart: actions.addToCart,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Products);
