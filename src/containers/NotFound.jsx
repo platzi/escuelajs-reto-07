e71403800e5dd32088d13a9d145accff0866258d
Erik Ricardo Sánchez Pérez <eriksape@MacBook-Pro-de-Erik.local>
2019-10-13 10:18:45 -0500

    first challenge
    added src/containers/NotFound.

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NotFound = () => (
+  <>
+    <div>No encontrado</div>
+    <Link to='/'>Regresar</Link>
+  </>
+)
+
+export default NotFound;
