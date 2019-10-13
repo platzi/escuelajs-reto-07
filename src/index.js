3e51a9455ea801fe88883f37ff53876f99cd128d
Jesús Antonio García Valadez <jesus.garciav@me.com>
2019-10-13 15:52:45 -0500

    feat: add support for service worker

--- src/index.js
+++ src/index.js
@@ -63,3 +63,10 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('app')
 );
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+    .catch(error => {
+      console.error(error);
+    });
+}
