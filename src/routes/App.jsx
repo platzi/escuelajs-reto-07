fc89297678ef55b48a62829e65ecc66d69a68fc2
Ingrid Sagrero <starlling@hotmail.com>
2019-10-12 20:25:41 -0500

    primer commit con solución al problema uno

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -10,6 +10,7 @@ const App = () => (
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/checkout" component={Checkout} />
+        <Route component={NotFound} />
       </Switch>
     </Layout>
   </BrowserRouter>
