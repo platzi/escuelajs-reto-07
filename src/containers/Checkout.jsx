9ac053d344a7fac528efaf2b0c20a7b4a92f96d5
Centli <yosoycentli@gmail.com>
2019-10-13 19:38:56 -0500

    Todos los retos (1-4) cumplidos

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import '../styles/components/Checkout.styl';
+import removeFromCart  from '../actions';
 
 const Checkout = (props) => {
   const { cart } = props;
+  const handleRemoveElement = (itemId) =>(
+    props.removeFromCart(itemId)
+  );
   return (
     <div className="Checkout">
       <div className="Checkout-content">
         {cart.length > 0 ? <h3>Lista de Pedidos:</h3> : <h2>Sin Pedidos</h2>}
         {cart.map(item => (
-          <div className="Checkout-item">
+          <div className="Checkout-item" key={item.key}>
             <div className="Checkout-element">
               <h4>{item.title}</h4>
               <span>
@@ -17,14 +21,17 @@ const Checkout = (props) => {
                 {item.price}
               </span>
             </div>
-            <i className="fas fa-trash-alt" />
+            <i className="fas fa-trash-alt" onClick={() => handleRemoveElement(item.key)} />
+          
           </div>
         ))}
       </div>
       {cart.length > 0 && (
         <div className="Checkout-sidebar">
           <h3>Precio Total:</h3>
-          <h4>$</h4>
+          <h4>$
+            {cart.reduce((suma, item) => (suma + item.price),0)}
+          </h4>
         </div>
       )}
     </div>
@@ -36,5 +43,11 @@ const mapStateToProps = state => {
     cart: state.cart,
   };
 };
+const mapDispatchToProps = {
+  removeFromCart,
+};
 
-export default connect(mapStateToProps, null)(Checkout);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Checkout);
