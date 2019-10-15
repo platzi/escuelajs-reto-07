5c27894bbc2ba61349a858bf80676deb03c212c0
guissmejia <nsierra@debian.debian>
2019-10-14 22:27:24 -0500

    Reto Sesion 7

--- src/containers/NotFound.jsx
+++ src/containers/NotFound.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import '../styles/components/NotFound.styl';
 
 const NotFound = () => {
-  <div className="not-found">
-    <h1> 404 </h1>
-    <h2>Pagina no encontrada</h2>
-  </div>
-};
+  return (
+    <div className="not-found">
+      <h1>Pagina no encontrada</h1>
+    </div>
+  );
+}
 
 export default NotFound;
