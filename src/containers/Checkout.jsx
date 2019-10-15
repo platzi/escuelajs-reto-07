c07f70e6318e9a0868a0515d15aba95d347ed485
Daniel <hola@dapino.co>
2019-10-15 00:37:10 -0500

    WIP: Challenge 3

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { removeFromCart } from '../actions'
 import '../styles/components/Checkout.styl';
 
 const Checkout = (props) => {
   const { cart } = props;
+
+  const handleRemoveFromCart = (product) => {
+    props.removeFromCart(product);
+  }
+
   return (
     <div className="Checkout">
       <div className="Checkout-content">
         {cart.length > 0 ? <h3>Lista de Pedidos:</h3> : <h2>Sin Pedidos</h2>}
         {cart.map(item => (
-          <div className="Checkout-item">
+          <div key={item.id} className="Checkout-item">
             <div className="Checkout-element">
               <h4>{item.title}</h4>
               <span>
@@ -17,7 +23,7 @@ const Checkout = (props) => {
                 {item.price}
               </span>
             </div>
-            <i className="fas fa-trash-alt" />
+            <button onClick={() => handleRemoveFromCart(item)} className="fas fa-trash-alt" />
           </div>
         ))}
       </div>
@@ -37,4 +43,8 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps, null)(Checkout);
+const mapDispatchToProps = {
+  removeFromCart,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Checkout);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,22 @@
 const reducer = (state, action) => {
+
   switch (action.type) {
     case 'ADD_TO_CART':
       return {
         ...state,
-        cart: [...state.cart, action.payload]
-      }
+        cart: [...state.cart, action.payload],
+      };
+
+    case 'REMOVE_FROM_CART':
+      return {
+        ...state,
+        cart: [
+          ...state.cart.slice( action.payload.index, action.payload.index ),
+        ],
+      };
     default:
       return state;
   }
-}
+};
 
 export default reducer;
