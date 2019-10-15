a5b4989557c1c6f5c828af9241858adca1d3157d
Jorge Humberto Nemoga Pinzon <jhnemogap@gmail.com>
2019-10-15 14:58:00 -0500

    First problem solved

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NotFound = () => (
+  <>
+    <h1>Error 404 - Page Not Found</h1>
+    <h2>
+      <Link to='/'>Volver al principio</Link>
+    </h2>
+  </>
+);
+
+export default NotFound;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -3,16 +3,20 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from '../containers/Home';
 import Checkout from '../containers/Checkout';
 import Layout from '../components/Layout';
+import NotFound from '../containers/NotFound';
 
-const App = () => (
-  <BrowserRouter>
-    <Layout>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/checkout" component={Checkout} />
-      </Switch>
-    </Layout>
-  </BrowserRouter>
-);
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/checkout' component={Checkout} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
+    </BrowserRouter>
+  );
+};
 
 export default App;
