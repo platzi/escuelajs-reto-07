61af7306a953d29d929f10657e5a9ae62d085e40
Rod Pasquel <rodpasquel@yahoo.com>
2019-10-16 00:07:52 -0500

    Solucion a Reto 7. 4 problemas resueltos.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,12 @@
-const addToCart = payload => ({
+export const deleteFromCart = payload => (
+  {
+      type: 'DELETE_FROM_CART',
+      payload,
+  }
+);
+
+export const addToCart = payload => ({
   type: 'ADD_TO_CART',
   payload,
 });
 
-export { addToCart };
--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import propTypes from 'prop-types';
+import { deleteFromCart } from '../actions';
 import '../styles/components/Checkout.styl';
 
 const Checkout = (props) => {
   const { cart } = props;
+  let totalPrice = 0;
+
+  const handleDeleteFromCart = (itemID) => {
+    props.deleteFromCart(itemID)
+  }
+  
+  // calculate total
+  cart.map(item => {
+    totalPrice = item.price + totalPrice;
+  });
+
   return (
     <div className="Checkout">
       <div className="Checkout-content">
         {cart.length > 0 ? <h3>Lista de Pedidos:</h3> : <h2>Sin Pedidos</h2>}
         {cart.map(item => (
-          <div className="Checkout-item">
+          <div className="Checkout-item" key={item.id}>
             <div className="Checkout-element">
               <h4>{item.title}</h4>
               <span>
@@ -17,15 +30,17 @@ const Checkout = (props) => {
                 {item.price}
               </span>
             </div>
-            <i className="fas fa-trash-alt" />
+            <i className="fas fa-trash-alt" onClick={() => handleDeleteFromCart(item.id)}/>
           </div>
         ))}
       </div>
       {cart.length > 0 && (
-        <div className="Checkout-sidebar">
-          <h3>Precio Total:</h3>
-          <h4>$</h4>
-        </div>
+          <div className="Checkout-sidebar">
+            <h3>Precio Total:</h3>
+            <h3> $
+              {totalPrice}
+            </h3>
+          </div>
       )}
     </div>
   )
@@ -37,4 +52,18 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps, null)(Checkout);
+Checkout.propTypes = {
+  id: propTypes.number, 
+  image: propTypes.string, 
+  title: propTypes.string, 
+  price: propTypes.number, 
+  description: propTypes.number,
+
+}
+
+
+const mapDispatchToProps = {
+  deleteFromCart,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Checkout);
