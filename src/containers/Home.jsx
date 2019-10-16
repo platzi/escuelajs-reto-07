1e1e41128a5ea39db0774b88fac2cf6120dc7a95
cristubuntu <cristhiam.tovar@mail.escuelaing.edu.co>
2019-10-16 13:46:06 -0500

    PlatziStore tested by Redux DevTools

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Products from '../components/Products';
 
 const Home = () => <Products />;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import App from './routes/App';
 import reducer from './reducers';
 
@@ -53,8 +53,8 @@ const initialState = {
 
   ]
 };
-
-const store = createStore(reducer, initialState);
+const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+const store = createStore(reducer, initialState, composeEnhancers());
 
 ReactDOM.render(
   <Provider store={store}>
