b1100860de20d2300db14775dbf0f013655087ae
luis_parra <luis.parra.pulido@gmail.com>
2019-10-16 10:25:56 -0500

    solución reto 7

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,6 +5,11 @@ const reducer = (state, action) => {
         ...state,
         cart: [...state.cart, action.payload]
       }
+    case 'REMOVE_OF_CART':
+      return {
+        ...state,
+        cart: state.cart.filter((products) => products.id !== action.payload),
+      }
     default:
       return state;
   }
