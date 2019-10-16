f8f1f018f8a04588713b3eb3cb5036dca9ac7887
saforus <1405pablo@gmail.com>
2019-10-15 23:05:55 -0500

    RETO 7

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import '../styles/components/NotFound.styl';
+
+const NotFound = () => (
+  <div>
+    <h1>Ups pagina no encontrada.</h1>
+  </div>
+);
+
+export default NotFound;
