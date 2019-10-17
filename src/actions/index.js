6b72e5724b402b897b01103c94d465387dabca6b
Francisco Angel <paconity@gmail.com>
2019-10-16 19:56:10 -0500

    Tercer Problema Resuelto

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,4 +3,9 @@ const addToCart = payload => ({
   payload,
 });
 
-export { addToCart };
+const deleteFromCart = payload => ({
+  type: 'DELETE_FROM_CART',
+  payload,
+});
+
+export { addToCart, deleteFromCart };
--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,25 +1,36 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react';
 import { connect } from 'react-redux';
+import { deleteFromCart } from '../actions';
 import '../styles/components/Checkout.styl';
 
 const Checkout = (props) => {
   const { cart } = props;
+  let ProdsIDs = 0;
+
+  const handelDeleteFromCart = (productId) => {
+    props.deleteFromCart(productId)
+  }
   return (
     <div className="Checkout">
       <div className="Checkout-content">
         {cart.length > 0 ? <h3>Lista de Pedidos:</h3> : <h2>Sin Pedidos</h2>}
-        {cart.map(item => (
-          <div className="Checkout-item">
-            <div className="Checkout-element">
-              <h4>{item.title}</h4>
-              <span>
+        {cart.map(item => {
+          // eslint-disable-next-line operator-assignment
+          ProdsIDs = ProdsIDs + 1;
+          return (
+            <div className="Checkout-item" key={ProdsIDs}>
+              <div className="Checkout-element">
+                <h4>{item.title}</h4>
+                <span>
                 $
-                {item.price}
-              </span>
+                  {item.price}
+                </span>
+              </div>
+              <i className="fas fa-trash-alt" onClick={() => handelDeleteFromCart(item.id)} />
             </div>
-            <i className="fas fa-trash-alt" />
-          </div>
-        ))}
+        )})}
       </div>
       {cart.length > 0 && (
         <div className="Checkout-sidebar">
@@ -37,4 +48,8 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps, null)(Checkout);
+const mapDispatchToProps = {
+  deleteFromCart,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Checkout);
