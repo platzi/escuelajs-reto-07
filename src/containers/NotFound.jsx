bee2c12ccb09aa84727645b832c623c2bba1d8ec
Inti <yeseniase@outlook.it>
2019-10-21 15:58:46 -0500

    ADD: solución 1er problema

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+
+const NotFound = () => (
+  <Fragment>
+    <h1> 404 </h1>
+    <h2> Página no encontrada</h2>
+  </Fragment>
+);
+
+export default NotFound;
